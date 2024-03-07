41f9d7318f7e15c84e7a016a9368732ff8e9e002
zak0chana <122036843+zak0chana@users.noreply.github.com>
2024-03-08 00:38:24 +0100

    Update command.py

--- src/command.py
+++ src/command.py
@@ -1,5 +1,5 @@
-from typing import Callable,Union
-from typing_extensions import Self
+from typing import Callable,Union,Type
+import inspect
 
 from .context import Context
 
@@ -9,19 +9,10 @@ def _EmptyFunc(ctx):
 
 
 class Command:
-    __invoked = False
-    def __init__(self,func):
-        self._func: Callable[[Context],None] = func
-        self.name = self._func.__name__ # function name
-        self.__orginal_name: str = ... # defined later; see __call__
 
-    @property
-    def orginal_name(self) -> str:
-        return self.__orginal_name # command name
-
-    @orginal_name.setter
-    def orginal_name(self,*args,**kwargs):
-        raise AttributeError("You cannot change orginal name")
+    def __init__(self,func,name):
+        self._func: Callable[[Context],None] = func # _FunctionType
+        self.name = name # command name
 
 
     def before_invoke(self,ctx:Context):
@@ -36,30 +27,20 @@ class Command:
         return _EmptyFunc(ctx)
 
 
-    def __call__(self, name) -> Union[Self,None]:
-        if not self.__invoked:
-            self.__invoked = True
-            self.__orginal_name = name
-            return self
-        raise AttributeError('function `__call__` can be invoke once')
+    def __eq__(self, other:Type["Command"]) -> bool:
+        """ Return self==value.
+        this function check attr: `name`, `_func`
+        """
+        if not hasattr(other, 'name'):
+            return self.name == other
+        return self.name == other.name
 
 
-    def __eq__(self, other:"Command"):
-        if not hasattr(other,'_func'):
-            return self._func == other
-        return self._func == other._func
-
-    def __ne__(self, other):
+    def __ne__(self, other) -> bool:
+        """ Return self!=value."""
         return not self.__eq__(other)
 
 
     def __repr__(self):
         name = self.name
-        return "Command(name={})".format(name)
-
-
-
-
-
-
-
+        return f"Command{name=})"
