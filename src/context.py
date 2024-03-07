c6926d8176e6920cca3bfe27a5381b3665ff2751
zak0chana <122036843+zak0chana@users.noreply.github.com>
2024-03-08 00:39:16 +0100

    Update context.py

--- src/context.py
+++ src/context.py
@@ -1,11 +1,16 @@
-from typing import Union, Optional
-import requests
-import requests_toolbelt
+import json
+import os
+from typing import Optional, Dict, Any
+
+from requests_toolbelt import MultipartEncoder
+
+from .http import HttpClient
 from .view import Buttons, QuicklyRieplies
 
-class Context:
+
+class Context(HttpClient):
     def __init__(self, **kwargs):
-        self._token: str = kwargs.get("token")  # token
+        self._token: str = kwargs.get("token")  # page access token
         self.sender: str = kwargs.get("sender_id")  # PSID of username
         self.command: str = kwargs.get("command")  # name of command
         self.username: str = kwargs.get("username")
@@ -13,18 +18,13 @@ class Context:
         self.created_time: str = kwargs.get("created_time")
         self.message_token_sender: str = kwargs.get("message_token_sender")  # useless ?
         self._api: str = kwargs.get("api")  # api version
-
-    def sendRequest(self, method: str, path: str, **kwargs) -> requests.Response:
-        url = "https://graph.facebook.com" + path
-        req = requests.request(method, url, **kwargs)
-        # req.raise_for_status()
-        return req
+        self._page_id: str = kwargs.get("page_id")  # page_id
 
     def send(self,
              text: str,
              buttons: Optional[Buttons] = None,
              quicklyRieplies: Optional[QuicklyRieplies] = None,
-             ):
+             ) -> Dict[str, Any]:
         """
         :param text: Must be less than 2000
         :param quicklyRieplies: An array of quick replies to be sent in a message
@@ -58,19 +58,57 @@ class Context:
         req = self.sendRequest("POST", "/me/messages", json=dataJSON, params=params)
         return req.json()
 
-    def send_image(self, url:str):
-        if not isinstance(url,str):
+    def sendMessegeAsPersona(self, text: str, id: str) -> Dict[str, Any]:  # TODO
+        """
+        :param text: text to send
+        :param id: persona id
+        :return: Dict[str,Any]
+        """
+        if not isinstance(text, str):
+            raise TypeError("Argument `text` must be str")
+        dataJSON = {'recipient': {'id': self.sender}, 'message': {'text': text}, 'persona_id': id,
+                    'access_token': self._token}
+        req = self.sendRequest('POST', f'/{self._api}/{self._page_id}/messages', json=dataJSON)
+        return req.json()
+
+    def send_image(self, url: str) -> Dict[str, Any]:
+        if not isinstance(url, str):
             raise TypeError('Argument `url` must be str')
 
         dataJSON = {"recipient": {"id": self.sender},
                     "message": {"attachment": {"type": "image", "payload": {"url": url, "is_reusable": False}}}}
         params = {"access_token": self._token}
 
-
         req = self.sendRequest("POST", "/me/messages", json=dataJSON, params=params)
         return req.json()
 
+    def send_local_file(self, filename: str):
+        if not isinstance(filename, str):
+            raise TypeError("Argument `filename` must be str")
 
+        request_body = MultipartEncoder({
+            "recipient": json.dumps({"id": self.sender}),
+            "message": json.dumps(
+                {
+                    "attachment": {
+                        "type": 'file',
+                        "payload": {
+                            "is_reusable": True,
+                        },
+                    }
+                }
+            ),
+            "filedata": (
+                os.path.basename(filename),
+                open(filename, "rb"),
+                f"file/{filename.split('.')[-1]}",
+            ),
+        })
+
+        headers = {"content-type": request_body.content_type}
+        params = {"access_token": self._token}
+        req = self.sendRequest("POST", '/me/messages', data=request_body, params=params, headers=headers)
+        return req.json()
 
     def __eq__(self, other) -> bool:
         """ Return self==value.
