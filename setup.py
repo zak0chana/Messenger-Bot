b8dd9494f4ff60e11f1659ebf9c1814ccce87985
zak0chana <122036843+zak0chana@users.noreply.github.com>
2024-03-08 01:05:04 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='MessengerApi',
+    version='1.0',
+    packages=find_packages(),
+    author_email='filipinek1125@gmail.com',
+    author='Jezyk',
+    requires=['requests']
+)
+
